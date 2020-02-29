55358cc6a1ae338e16f2c7628cd8aa2b06ec04c0
szewski <you@example.com>
2020-02-29 12:43:59 +0100

    Items are listed on catalog page.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-## CodeMe Python Zaawansowany
-### Projekt internetowej biblioteki
+﻿#### CodeMe Python Zaawansowany
+## Projekt internetowej biblioteki
+
+#### Podsumowanie
+Aplikacja biblioteki służy użytkownikom do zarządzania zasobami, zamawiania pozycji oraz sprawdzania stanu konta.
+
+#### Możliwości aplikacji:
+- rejestracja
+- logowanie
+- podział na użytkowników:
+  - Guest side
+    - przeglądanie
+
+  - User side
+    - przeglądanie
+    - zamawianie
+    - sprawdzenie stanu konta
+    - przedłużenie wypożyczenia (bonus)
+
+  - Admin side
+    - przeglądanie
+    - podgląd stanu konta użytkowników
+    - akceptowanie zamówienia - wypożyczenie przygotowane (bonus)
+    - akceptacja przedłużenia wypożyczenia (bonus)
+    - dodawanie nowych pozycji
+    - edytowanie pozycji
+
+#### Backend
+- server Flask
+- dane zapisane w SQLite3
+- szablony Jinja2
--- flask-library.py
+++ flask-library.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, url_for
+import json
 
 app = Flask(__name__)
+
+with open('database.json', 'r') as f:
+    books = json.load(f)
+    books = books['books']
 
 
 @app.route('/')
@@ -11,13 +16,29 @@ def home():
 
 @app.route('/login')
 def login():
-    return render_template('login.html', title='Login')
+    return render_template('login.html', title='Zaloguj')
 
 
 @app.route('/register')
 def register():
-    return render_template('register.html', title='Register')
+    return render_template('register.html', title='Rejestracja')
+
+
+@app.route('/account')
+def account():
+    return render_template('account.html', title='Konto')
+
+
+@app.route('/catalog')
+def catalog():
+    return render_template('catalog.html', title='Katalog', books=books)
+
+
+@app.route('/item')
+def item():
+    return render_template('item.html', title='Pozycja')
 
 
 if __name__ == '__main__':
+    # pass
     app.run(debug=True)
