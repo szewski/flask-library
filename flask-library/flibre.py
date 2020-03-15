4bd506f02822b67d8d64e142f2833d9fbb57f14d
szewski <you@example.com>
2020-03-15 14:53:34 +0100

    Write to database Add item page with form Catalog page with list of books from database

--- flask-library/flibre.py
+++ flask-library/flibre.py
@@ -1,18 +1,40 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
+from database import Database
 
 
 app = Flask(__name__)
+db = Database('database.json')
 
 
 @app.route('/')
-@app.route('/home', methods=['GET'])
+@app.route('/home')
 def home():
     return render_template('index.html')
 
 
-@app.route('/library', methods=['GET'])
-def library():
-    return render_template('library.html')
+@app.route('/catalog')
+def catalog():
+    data = db.read()
+    books = data['books']
+
+    context = {'books': books}
+    return render_template('catalog.html', **context)
+
+
+@app.route('/catalog/add_item')
+def add_item():
+    return render_template('add_item.html')
+
+
+@app.route('/catalog/add_item_post', methods=['POST'])
+def add_item_post():
+    new_book = request.form.to_dict()
+    data = db.read()
+
+    data['books'].append(new_book)
+    db.write(data)
+
+    return f"Book {new_book['title']} added!"
 
 
 if __name__ == '__main__':
